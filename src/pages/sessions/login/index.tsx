c2aff65c8f66244e33c80169723f37fc990d003d
Minh Hy <hyaminh2303@gmail.com>
2023-04-18 16:41:51 +0700

    Init feature forgot password

--- src/pages/sessions/login/index.tsx
+++ src/pages/sessions/login/index.tsx
@@ -1,4 +1,3 @@
-// src/LoginPage.tsx
 import * as React from 'react';
 import { useState } from 'react';
 import { useLogin, useNotify, Notification, Login, LoginFormProps } from 'react-admin';
@@ -47,10 +46,68 @@ const LoginForm: React.FC<LoginFormProps> = (props) => {
   );
 };
 
+const ForgotPasswordForm: React.FC = (props) => {
+  const [email, setEmail] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const notify = useNotify();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      console.log(email)
+      await Auth.forgotPassword(email);
+      setSubmitted(true);
+    } catch (error) {
+      notify('An error occurred while resetting your password');
+    }
+  };
+
+  if (submitted) {
+    return <p>Instructions to reset your password have been sent to your email address</p>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        required
+        fullWidth
+        autoFocus
+      />
+      <Button type="submit" color="primary" variant="contained">
+        Reset Password
+      </Button>
+      <Notification />
+    </form>
+  );
+};
+
 const LoginPage: React.FC = (props) => {
+  const [forgotPassword, setForgotPassword] = useState(false);
+
+  const handleForgotPasswordClick = () => {
+    setForgotPassword(true);
+  };
+
+  const handleBackToLoginClick = () => {
+    setForgotPassword(false);
+  };
+
+  if (forgotPassword) {
+    return (
+      <>
+        <Button onClick={handleBackToLoginClick}>Back to Login</Button>
+        <ForgotPasswordForm />
+      </>
+    );
+  }
+
   return (
     <Login {...props}>
       <LoginForm />
+      <Button onClick={handleForgotPasswordClick}>Forgot Password</Button>
     </Login>
   );
 };
